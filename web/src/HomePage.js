fb11ca2e2def30f3eab8552fc4c5ba6763f890c0
Rona Sheen <rsheen@usc.edu>
2016-03-14 23:18:00 -0700

    Game maps component created. Tweaked.

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -30,7 +30,7 @@ var HomePage = React.createClass({
                          <svg className="gamefeed-svg"><img src="/assets/icons/fingerprint.svg" alt="fingerprint icon" className="icon" /></svg>
                      </div>
                      <div className="divFeedColumn2 column">
-                         <p className="suggest-body"><span className="suggested">EVKitty</span> suggested <span className="suggested">George Tirebiter</span> with the <span className="suggested">empty beer cans</span> at <span className="suggested">Lyon Center</span>.</p>
+                         <p className="suggest-body"><span className="suggested">EVKitty</span> suggested <span className="suggested">George Tirebiter</span> with the <span className="suggested">empty beer cans</span> at <span className="suggested">Leavey Library</span>.</p>
                      </div>
                      <div className="divFeedColumn3 column">5 minutes ago </div>
                  </div>
@@ -41,7 +41,7 @@ var HomePage = React.createClass({
                      </div>
 
                      <div className="divFeedColumn2 column">
-                         <p className="suggest-body"><span className="suggested">EVKitty</span> suggested <span className="suggested">George Tirebiter</span> with the <span className="suggested">empty beer cans</span> at <span className="suggested">Lyon Center</span>.</p>
+                         <p className="suggest-body"><span className="suggested">Tommy Trojan</span> suggested <span className="suggested">George Tirebiter</span> with the <span className="suggested">Viterbi classes</span> at <span className="suggested">Leavey Library</span>.</p>
                      </div>
                      <div className="divFeedColumn3 column">5 minutes ago </div>
                  </div>
@@ -52,7 +52,7 @@ var HomePage = React.createClass({
                      </div>
 
                      <div className="divFeedColumn2 column">
-                         <p className="suggest-body"><span className="suggested">EVKitty</span> suggested <span className="suggested">George Tirebiter</span> with the <span className="suggested">empty beer cans</span> at <span className="suggested">Lyon Center</span>.</p>
+                         <p className="suggest-body"><span className="suggested">President Nikias</span> suggested <span className="suggested">Pete Carroll</span> with the <span className="suggested">U-lock</span> at <span className="suggested">The Row</span>.</p>
                      </div>
                      <div className="divFeedColumn3 column">5 minutes ago </div>
                  </div>
