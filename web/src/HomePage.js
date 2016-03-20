74896ac49219a2ce054e147b309168a1b39ff921
Rona Sheen <rsheen@usc.edu>
2016-03-20 13:55:40 -0700

    Trying to make a modal for Suggest

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -184,28 +184,6 @@ nextPage: function(e) {
   }
 });
 
-var Modal = require('boron/FadeModal');
-var Example = React.createClass({
-    showModal: function(){
-        this.refs.modal.show();
-    },
-    hideModal: function(){
-        this.refs.modal.hide();
-    },
-    render: function() {
-        return (
-            <div>
-                <button onClick={this.showModal}>Open</button>
-                <Modal ref="modal">
-                    <h2>I am a dialog</h2>
-                    <button onClick={this.hideModal}>Close</button>
-                </Modal>
-            </div>
-        );
-    }
-});
-
-
 
 module.exports = HomePage;
 
