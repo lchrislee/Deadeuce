88f949cdb644a0cd26a2dd465652388e90722d14
Michelle <mtiu@usc.edu>
2016-03-23 17:28:50 -0700

    nav bar updated with links

--- web/src/NavBar.js
+++ web/src/NavBar.js
@@ -5,11 +5,11 @@ var NavBar = React.createClass({
     render: function(){
         return(
             <div className="navBar">
-                <p>Deadeuce</p>
+                <p><a href="../static/index.html">Deadeuce</a></p>
                 <ul className="navLinks">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Games</a></li>
-                    <li><a href="">Profile</a></li>
+                    <li><a href="../static/index.html">Home</a></li>
+                    <li><a href="../static/games.html">Games</a></li>
+                    <li><a href="../static/profile.html">Profile</a></li>
                 </ul>
             </div>
         )
