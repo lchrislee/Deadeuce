e6dd2a7cd8e054486d83e62343c490ed787cab0d
Michael Young <youn462@usc.edu>
2016-03-30 23:16:47 -0700

    began porting components to the correct pages

--- web/src/pages/CreateGame.js
+++ web/src/pages/CreateGame.js
@@ -2,22 +2,47 @@ var React = require('react');
 var $ = require('jQuery');
 var Router = require('react-router');
 var Link = require('react-router').Link;
+var CreateGame2 = require('../components/CreateGame2')
 
 
 var CreateGame = React.createClass({
-	getInitialState: function() {
-    return {
-    };
+  mixins: [
+    require('react-onclickoutside')
+  ],
+  handleClickOutside: function(evt) {
+    this.props.closeModal(evt);
+  },
+  closeModal: function(evt) {
+    this.props.closeModal(evt);
   },
   render: function() {
     return (
-     <div>
-     	<div>This is the Create Game page!</div>
-      <br/>
+      <div className="example-modal">
+        <h1> Pick a Character </h1>
+        <button onClick={this.closeModal} className="close">X</button>
+        <form>
+          <table>
+          <tbody>
+            <tr>
+              <th>The murder has taken place. It is now up to you, 
+                <select name="selectCharacter" defaultValue="EV Kitty"> 
+                  <option>EV Kitty</option>
+                  <option>Tommy Trojan</option>
+                  <option>George Tirebiter</option>
+                  <option>President Nikias</option>
+                  <option>Pete Carroll</option>
+                </select>
+              and your friends to solve the crime! <br/> 
+              </th>
+            </tr>
+            </tbody>
+            </table>
+                <input type="submit" name="submitCharacter" defaultValue="invite friends" />
+        </form>  
       <Link to="game_home" activeClassName="active">Game Home</Link>
-     </div>
-    );
-  }
+      </div>
+  );
+  },
 });
 
 module.exports = CreateGame;
