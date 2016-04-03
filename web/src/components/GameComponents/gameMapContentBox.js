cf759973e1bc05b34910c6779151c863f2dee0d0
Rona Sheen <rsheen@usc.edu>
2016-04-03 13:05:50 -0700

    new js

---
+++ web/src/components/GameComponents/gameMapContentBox.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Rona on 4/3/16.
+ */
+var React = require('react');
+var $ = require('jQuery');
+
+var GameMap = React.createClass({
+    getInitialState: function() {
+        return{
+
+        };
+    },
+    render: function(){
+        var playerLocations = [];
+        if(this.props.playerLocations.location == )
+
+        return(
+            <div className="mapBox">
+                <div className="gameMapBoxHeader">
+                <p className="mapHeader"></p>
+                </div>
+
+                <div className="gameMapBoxBody">
+                    <p className="mapContent"></p>
+                </div>
+
+            </div>
+        );
+    }
+});
+
+module.exports = GameMapContentBox;
