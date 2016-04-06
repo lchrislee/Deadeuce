807869e39ff9b95fc21d3796acfbb30825442456
MicYoung <Young462@usc.edu>
2016-04-05 20:54:35 -0700

    merge conflict fixed

--- web/src/pages/CreateGame.js
+++ web/src/pages/CreateGame.js
@@ -2,12 +2,8 @@ var React = require('react');
 var $ = require('jQuery');
 var Router = require('react-router');
 var Link = require('react-router').Link;
-<<<<<<< HEAD
 var CreateGameForm = require('../components/CreateGame/CreateGameForm');
 
-=======
-var CreateGameForm = require('../components/CreateGame/CreateGameForm.js');
->>>>>>> d8608f73ccce6fab131b858c094e2c009e9b1358
 
 var CreateGame = React.createClass({
   render: function() {
