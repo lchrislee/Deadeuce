7c8f8c56e68a93de76de967910cd911902cf8c2b
MYoung25 <youn462@usc.edu>
2016-04-06 01:01:20 -0700

    added links to the buttons

--- web/src/components/Feedback/FeedbackWrong.js
+++ web/src/components/Feedback/FeedbackWrong.js
@@ -1,16 +1,26 @@
 var React = require('react');
+var Router = require('react-router');
 var $ = require('jQuery');
 
-var FeedbackWrong = React.createClass({ 
+var FeedbackWrong = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  detectivePad: function(){
+    this.context.router.push('game_home/detective_pad');
+  },
+  gameHomeReturn: function(){
+    this.context.router.push('game_home');
+  },
   render: function() {
-    return (
-	    <div className="feedbackContent">
-            <p className="feedback-header"><br/>SUGGESTION FEEDBACK</p>
-            <p>No one had any clues to help you!</p>
+  return (
+	  <div className="feedbackContent">
+      <p className="feedback-header"><br/>SUGGESTION FEEDBACK</p>
+      <p>No one had any clues to help you!</p>
 
-            <button className="feedback-button" type="button">DETECTIVE CHECKLIST</button>
-            <br/><br/>
-            <button className="feedback-button" type="button">RETURN TO GAME</button>
+      <button className="feedback-button" type="button" onClick={this.detectivePad}>DETECTIVE CHECKLIST</button>
+      <br/><br/>
+      <button className="feedback-button" type="button" onClick={this.gameHomeReturn}>RETURN TO GAME</button>
 	    </div>
 	);
   },
