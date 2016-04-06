b4d54b0f0a7e56f7ac13726a8a1c5527271144e0
Zoe LaPorte <zlaporte@usc.edu>
2016-04-05 21:58:16 -0700

    added ALL GAMES module to Join Game page

--- web/src/pages/JoinGame.js
+++ web/src/pages/JoinGame.js
@@ -2,6 +2,7 @@ var React = require('react');
 var $ = require('jQuery');
 var Router = require('react-router');
 var Link = require('react-router').Link;
+var JoinGameModalContent = require('../components/JoinGame/JoinGameModalContent.js')
 
 
 var JoinGame = React.createClass({
@@ -12,12 +13,9 @@ var JoinGame = React.createClass({
   render: function() {
     return (
      <div>
-      <div>This is the join game page!</div>
       <br/>
+      <JoinGameModalContent />
       <Link to="game_home" activeClassName="active">Game Home</Link>
-      
-      
-
      </div>
     );
   }
