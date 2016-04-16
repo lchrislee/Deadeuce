812b833c293fba2e00592501daa8d9017431da83
Rona Sheen <rsheen@usc.edu>
2016-04-16 16:31:57 -0700

    Fixed Available Games stuff Edits to GameFeed content so that the date generates now

--- web/src/components/GameComponents/availableGamesContent.js
+++ web/src/components/GameComponents/availableGamesContent.js
@@ -8,13 +8,39 @@ var AvailableGamesContent = React.createClass({
     },
     getInitialState: function(){
         return{
-
+            "isSelected": false
         };
     },
+    handleClick: function() {
+       if (this.state.isSelected == true){
+            this.setState({
+                "isSelected": false
+            })
+        } else {
+            this.setState({
+                "isSelected": true
+            })
+        }
+    },
+    //selected: function(obj){
+    //    console.log(obj);
+    //    obj.style.backgroundColor = '#444';
+    //    obj.style.color = 'white';
+    //},
     render: function(){
-
+        var isSelected = this.state.isSelected;
+        var style = {};
+        if (isSelected) {
+            style = {
+                'background-color': '#444'
+            };
+        } else {
+            style = {
+                'background-color': '#CCC'
+            };
+        }
         return(
-            <div className="divFeedRow">
+            <div className="divFeedRow" onClick={this.handleClick} style={style}>
                 <div className="divFeedColumn1 column" >
                     <svg className="gamefeed-svg"><img src="/assets/icons/fingerprint.svg" alt="fingerprint icon" className="icon" /></svg>
                 </div>
