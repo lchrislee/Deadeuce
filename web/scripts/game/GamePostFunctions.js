a561fced261a0c8ad5f444de9cf6870e27990ddc
MYoung25 <youn462@usc.edu>
2016-04-18 20:45:19 -0700

    merge conflict

--- web/scripts/game/GamePostFunctions.js
+++ web/scripts/game/GamePostFunctions.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 var UserSchema = require("../../models/user.js");
 var GameSchema = require("../../models/game.js");
 
-<<<<<<< HEAD
 var checkList = {
   "locations":[
     "Lyon Center",
@@ -109,63 +108,6 @@ exports.createGame = function(mongooseDB, hostEmail, gameName){
       });
     }
   });
-=======
-exports.createGame = function(db){
-  return {gameID: 1234};
-	// var cantCreate = false;
- //  gameInfo= {
- //    "_id" : String(gameIDValues++),
- //    "title": gameName,
- //    "weapons" : ["Empty Soda Cans",
- //                 "Viterbi Finals",
- //                 "U-Lock",
- //                 "Tommy Trojan's Sword",
- //                 "Dining Hall Food",
- //                 "Freshman's Longboard"],
- //    "locations" : ["Ground Zero",
- //                   "EVK",
- //                   "Lyon Center",
- //                   "Leavey Library",
- //                   "Traddies",
- //                   "The 90",
- //                   "Bovard",
- //                   "The Row",
- //                   "Campus Center"],
- //    "turnIndex": 0,
- //    "users" : {hostID
- //              },
- //    "answer": {"location":"EVK",
- //               "weapon":"U-Lock",
- //               "user":hostID}//,
- //    // "usersId":[hostID]
- //  };
-
- //  function createGameEntry(){
- //    db.collection('game').insertOne(gameInfo, function(err, resultGame) {
- //      console.log(resultGame);
- //      var result = {};
- //      if(!err){
- //        result['gameID'] = resultGame.ops[0]._id;
- //        return result;
- //      }else{
- //        return result;
- //      }
- //    });
- //  };
-
- //  db.collection('game').find({"title":gameInfo.title}).count(function(err, count) {
- //    var result = {};
- //    if(count >= 1){
- //      cantCreate = true;
- //    }
- //    if(cantCreate){
- //      result['result'] = "failed";
- //      return result;
- //    }else{
- //      return createGameEntry();
- //    }
- //  });
->>>>>>> master
 }
 
 
